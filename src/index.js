15390584a49e0f09f78c51bb67ac10d02f00ddeb
Lukas Sagmani <lsagmani2003@gmail.com>
2025-02-08 16:21:05 -0500

    Pushing

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import AIChatBot_LoginPage from "./AIChatBot_LoginPage"; //Change: import App from './App';
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <AIChatBot_LoginPage /> {/* Change: <App />  */}
   </React.StrictMode>
 );
 
