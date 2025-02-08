565289f9f6f4381425f7e60ac99216eb524c1c43
Nuri Hannan <nurihannan@Nuris-MacBook-Air.local>
2025-02-08 17:03:49 -0500

    pushing

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+//import App from './App';
+import AiChatBot_HomePage from './AiChatBot_HomePage';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <AiChatBot_HomePage />
   </React.StrictMode>
 );
 
