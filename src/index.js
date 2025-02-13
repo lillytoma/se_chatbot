0b0e54cb1836ea2e936da5a471d6d83e44322218
Lukas Sagmani <lsagmani2003@gmail.com>
2025-02-12 19:19:13 -0500

    update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from './App';
-
+import App from "./App";
 
 import AiChatBot_HomePage from "./AiChatBot_HomePage";
 import AiChatBot_LoginPage from "./AIChatBot_LoginPage";
@@ -14,5 +13,3 @@ root.render(
     <AiChatBot_LoginPage />
   </React.StrictMode>
 );
-
-
