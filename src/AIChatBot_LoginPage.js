c741620bfcfa8a3148d46729986727533ffc977f
lillytoma <lilyanntoma@icloud.com>
2025-02-18 14:13:24 -0500

    pattern format in html for login page

--- src/AIChatBot_LoginPage.js
+++ src/AIChatBot_LoginPage.js
@@ -2,16 +2,23 @@ import "./AIChatBot_LoginPage.css";
 import { useState } from "react";
 import ReactDOM from "react-dom/client";
 
+// grabs the UserLogin data
+// db.collection('UserLogins').get().then(() =>{
+//   snapshot.docs.forEach(doc => {
+//     console.log(doc.data())
+//   })
+// })
+
 function LoginButton() {
   //Created a function for login button
 
-  return <button class="buttonCSS LoginButtonCentering">Login</button>; //created login button
+  return <button class="LoginButton">Login</button>; //created login button
 } //login button function ends
 
 function SignUpButton() {
   //Created a function for sign up button
   return (
-    <button class="buttonCSS SignUpButtonCentering">Sign Up</button>
+    <button class="SignUpButton">Sign Up</button>
     //created sign up button
   );
 } //sign up button function ends
@@ -22,25 +29,28 @@ function AIChatBot_LoginPage() {
   return (
     // need this to return attriibuites
 
-    <div>
-      {" "}
+    <div class = "Login_Content">
+      
       {/* need div to return multiple attribuites, without div i can only use on attribuite */}
       <body class="backroundColor">
-        {" "}
+     
         {/* Body for backround color*/}
-        <form>
+        <form id = "User_Login">
           <label class="CenterEmail">
             <h3>Enter Email:</h3>
-            <input type="Email" />
+            <input type="Email"
+            pattern = "[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$"/> 
+            {/* After the "." sign, add at least 2 letters from a to z */}
             {/* input created to ask user for input on email*/}
           </label>
           <label class="CenterPassword">
             <h3>Enter Password:</h3>
-            <input type="password" />
-            {/* input created to ask user for input on password*/}
+            <input type="password" 
+            pattern= "[?=*!A-Za-z]"/> 
+            {/* input created to ask user for input on password no special characters such as @#().. etc*/}
           </label>
           <LoginButton /> {/* This is the login button*/}
-        </form>{" "}
+        </form>
         <SignUpButton /> {/* This is the sign in button*/}
       </body>
     </div>
