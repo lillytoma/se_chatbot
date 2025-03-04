4aee74f5dd4ba8bf437ac63a43781056fd50e561
Nuri Hannan <nurihannan@Nuris-MacBook-Air.local>
2025-03-04 17:12:51 -0500

    fix

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from "react";
+//import ReactDOM from "react-dom/client";
 import ReactDOM from "react-dom/client";
+
+// import AIChatBotSignUpPage from "./AIChatBotSignUpPage";
 import App from "./App";
-
-
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-    {/* <Comp/> */}
-    {/* <AIChatBotHomePage/> */}
-    {/* <AIChatBotSignUpPage /> */}
   </React.StrictMode>
 );
