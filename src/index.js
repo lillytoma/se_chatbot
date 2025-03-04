049eaedd5aaef7c5868b14eaa328a6ab04527b85
Lilly Toma <lillytoma@Lillys-MacBook-Air-2.local>
2025-03-04 16:54:20 -0500

    idk

--- src/index.js
+++ src/index.js
@@ -7,12 +7,9 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-<<<<<<< HEAD
-=======
     <App />
     {/* <Comp/> */}
     {/* <AIChatBotHomePage/> */}
->>>>>>> 43fb83cbd03e51ad9a62795a9fd7b6e9509acdb3
     {/* <AIChatBotSignUpPage /> */}
     {<App />}
   </React.StrictMode>
