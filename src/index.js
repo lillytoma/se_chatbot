77ea3af36783d4881b725f366ccc9a7fc912260f
Lilly Toma <lillytoma@Lillys-MacBook-Air-2.local>
2025-03-04 14:26:07 -0500

    changed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import AIChatBotSignUpPage from "./AIChatBotSignUpPage";
+
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <AIChatBotSignUpPage /> */}
     <App />
+    {/* <Comp/> */}
+    {/* <AIChatBotHomePage/> */}
+    {/* <AIChatBotSignUpPage /> */}
   </React.StrictMode>
 );
