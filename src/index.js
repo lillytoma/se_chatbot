78da27f7c21d37716089eedaf501353ea8f9ff2d
Lukas Sagmani <lsagmani2003@gmail.com>
2025-04-08 16:50:09 -0400

    changes

--- src/index.js
+++ src/index.js
@@ -8,16 +8,11 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-<<<<<<< HEAD
+
     {/* <Comp/> */}
     {/* <AIChatBotHomePage/> */}
     {/* <AIChatBotSignUpPage /> */}
-<<<<<<< HEAD
-=======
-    {<App />}
->>>>>>> c4af56135b4ba0cb203396e8174e394a11cbc159
-=======
 
->>>>>>> f914b016fdd552fba1a59b6e9e7906e5b9af7793
+    {<App />}
   </React.StrictMode>
 );
